c59de6694833b80e4de18fefd37af450f30f85d3
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-08-22 12:25:05 +0200

    DarkMode component

---
+++ src/lib/components/themeSwitch.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { onMount } from "svelte"
+
+let isDarkMode = false;
+
+onMount(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        toggleDarkMode();
+    }
+});
+
+export function toggleDarkMode() {
+    isDarkMode = !isDarkMode;
+
+    if (isDarkMode) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    }
+}
+</script>
+
