2e86c869b087a5c7cd5bfe865cac4b886859b736
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-08-31 13:36:18 +0200

    Expanded for CV

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,6 +2,7 @@
     import { onMount } from 'svelte'
 
     let isDarkMode = false
+    let isExpanded = false
 
     onMount(() => {
         const savedTheme = localStorage.getItem('theme')
@@ -21,10 +22,29 @@
             localStorage.setItem('theme', 'light')
         }
     }
+
+    function dropdown() {
+        isExpanded = !isExpanded
+    }
+    
 </script>
 
 <div class="my-2 mx-4 flex items-center justify-between text-2xl font-bold sm:text-4xl">
-    <a href="https://www.google.com/" rel="noreferrer" target="_blank"><i class="bi bi-file-earmark-person" /></a>
+    <div class="relative z-10">
+        <button class="bi bi-file-earmark-person" on:click={dropdown}></button>
+        {#if isExpanded}
+        <div class="absolute shadow-lg rounded bg-indigo-800 m-1 text-2xl p-1 w-16 flex-col mx-auto flex items-center ">
+            <a href="/pdf/CV_Alex_esp.pdf" target="_blank">
+                <button class="rounded m-1 bg-violet-400 w-12">ESP</button>
+            </a>
+                <a href="/pdf/CV_Alex_en.pdf" target="_blank">
+                <button class="rounded m-1 bg-violet-400 w-12">EN</button>
+            </a>
+
+        </div>
+        {/if}
+    </div>
+
 
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <button
